987f386f8eeb6c4bf8ffad9a63cb9a716435ebbe
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-04-14 13:01:56 +0100

    Added filtering categories

--- products/views.py
+++ products/views.py
@@ -3,10 +3,11 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from django.db.models import Q
 from django.views.generic import TemplateView,ListView,DetailView
-from .models import Product
+from .models import Product,Category
 
 
 # Create your views here.
+
 
 
 class ProductListView(ListView):
@@ -14,27 +15,31 @@ class ProductListView(ListView):
     template_name = "products/products.html"
     context_object_name = 'products_list'
 
-    def get_queryset(self):
+    def get_queryset(self):  # sourcery skip: use-named-expression
+        queryset = self.model.objects.all()
+
+        # Filter by category
+        categories = self.request.GET.get('category')
+        if categories:
+            categories = categories.split(',')
+            queryset = queryset.filter(category__name__in=categories)
+
+        # Filter by search query
         query = self.request.GET.get('q')
         if query:
             queries = Q(name__icontains=query) | Q(description__icontains=query)
-            return self.model.objects.filter(queries)
-        else:
-            return self.model.objects.all()
-        
+            queryset = queryset.filter(queries)
 
-    def get(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        if not queryset.exists():
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse_lazy('products'))
-        return super().get(request, *args, **kwargs)
+        return queryset
 
-"""  def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        
+        # Add search term and current categories to context
         context['search_term'] = self.request.GET.get('q')
-        return context """
-
+        context['current_categories'] = Category.objects.filter(name__in=self.request.GET.get('category', '').split(','))
+        
+        return context
    
 
 class ProductDetailView(DetailView):
