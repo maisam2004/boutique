972e865b47ed20e31284939092b3ec0930187a91
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-04-16 16:30:49 +0100

    Upadate shopping bag

--- bag/urls.py
+++ bag/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.ViewBagTemplateView.as_view(),name='view_bag'),
+    path('add/<int:item_id>/', views.AddToBagView.as_view(), name='add_to_bag'),
 
 
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView,View
 
 
 # Create your views here.
@@ -7,3 +7,23 @@ from django.views.generic import TemplateView
 class ViewBagTemplateView(TemplateView):
     """ a View to return the index page """
     template_name = 'bag/bag.html'
+
+
+class AddToBagView(View):
+    def post(self, request, item_id):
+        """ Add a quantity of the specified product to the shopping bag """
+        
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+        
+        bag = request.session.get('bag', {})
+
+        if item_id in bag:
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
+        request.session['bag'] = bag
+        # Print bag contents to console
+        print("Current shopping bag contents:", request.session['bag'])
+        return redirect(redirect_url)
